9676faca00e2febb288898cf058534604f4b98bf
sam <samhodgkinson9192@gmail.com>
2024-11-21 19:32:11 +0000

    Commented out cmp.lua for now, might want to use it again later, but replaced it (at least the stuff I was using it for) with emmet language server and nvim-emmet... Think this is better for me... Wait out...

--- lua/custom/plugins/mason.lua
+++ lua/custom/plugins/mason.lua
@@ -39,29 +39,55 @@ return {
 
             -- Set up LSP servers
             local lspconfig = require('lspconfig')
-            
+
             -- Capabilities for better completion
             local capabilities = vim.lsp.protocol.make_client_capabilities()
-            
+
             -- Set up each LSP server
             lspconfig.lua_ls.setup({
                 capabilities = capabilities,
             })
-            
+
             lspconfig.ts_ls.setup({
                 capabilities = capabilities,
             })
-            
+
             lspconfig.gopls.setup({
                 capabilities = capabilities,
             })
-            
+
             lspconfig.cssls.setup({
                 capabilities = capabilities,
             })
-            
+
             lspconfig.html.setup({
                 capabilities = capabilities,
+            })
+
+            lspconfig.emmet_language_server.setup({
+              filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" },
+              -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+              -- **Note:** only the options listed in the table are supported.
+              init_options = {
+                ---@type table<string, string>
+                includeLanguages = {},
+                --- @type string[]
+                excludeLanguages = {},
+                --- @type string[]
+                extensionsPath = {},
+                --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+                preferences = {},
+                --- @type boolean Defaults to `true`
+                showAbbreviationSuggestions = true,
+                --- @type "always" | "never" Defaults to `"always"`
+                showExpandedAbbreviation = "always",
+                --- @type boolean Defaults to `false`
+                showSuggestionsAsSnippets = false,
+                --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+                syntaxProfiles = {},
+                --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+                variables = {},
+              },
             })
 
             -- Global LSP keybindings
