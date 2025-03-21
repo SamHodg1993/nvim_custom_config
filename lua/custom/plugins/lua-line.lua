8c19da8672e43d1646ec2b5227779693ee30dd1d
samhodg1993 <samhodgkinson9192@gmail.com>
2025-03-21 09:15:49 +0000

    Added the longer filepath back to lua line, it's deffo not as clean, but it's essential for me

--- lua/custom/plugins/lua-line.lua
+++ lua/custom/plugins/lua-line.lua
@@ -22,7 +22,7 @@ return {
             sections = {
                 lualine_a = { 'mode' },
                 lualine_b = { 'branch', 'diff', 'diagnostics' },
-                lualine_c = { 'filename' },
+                lualine_c = { { 'filename', path = 1 } },
                 lualine_x = { recording_status, 'encoding', 'fileformat', 'filetype' },
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' }
@@ -30,3 +30,4 @@ return {
         })
     end
 }
+
