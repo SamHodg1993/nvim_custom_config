03301ccc28f560a76aab71519c08a1e39e4c3153
SamHodg1993 <samhodgkinson9192@gmail.com>
2025-06-18 12:55:16 +0100

    Added keybinds to the new diffview package.

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -12,6 +12,32 @@ return {
 					layout = "diff3_mixed", -- Shows base, left, right in 3-way merge
 				},
 			},
+
+			-- Add keymaps configuration
+			keymaps = {
+				view = {
+					-- Add any view-specific mappings here
+				},
+				file_panel = {
+					-- Add any file panel specific mappings here
+				},
+			},
 		})
+
+		-- Set up global keybindings for Diffview commands
+		local keymap = vim.keymap.set
+
+		-- Normal mode mappings with <leader>d prefix for diff operations
+		keymap("n", "<leader>mo", ":DiffviewOpen<CR>", { desc = "DiffviewOpen" })
+		keymap("n", "<leader>mc", ":DiffviewClose<CR>", { desc = "DiffviewClose" })
+		keymap("n", "<leader>mt", ":DiffviewToggleFiles<CR>", { desc = "DiffviewToggleFiles" })
+		keymap("n", "<leader>mf", ":DiffviewFocusFiles<CR>", { desc = "DiffviewFocusFiles" })
+		keymap("n", "<leader>mr", ":DiffviewRefresh<CR>", { desc = "DiffviewRefresh" })
+
+		-- If you want to add a keybind for viewing diff with HEAD
+		keymap("n", "<leader>mh", ":DiffviewOpen HEAD<CR>", { desc = "DiffviewOpen HEAD" })
+
+		-- If you want to add a keybind for viewing diff with specific commit
+		keymap("n", "<leader>ml", ":DiffviewOpen HEAD~1<CR>", { desc = "DiffviewOpen last commit" })
 	end,
 }
